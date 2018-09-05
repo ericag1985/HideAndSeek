a01ce8b6da0538686b64f8ef1e76384d49d1e0ce
Erica Gugliemella <erica.gugliemella@isobar.com>
2018-09-05 13:36:48 -0400

    Removing old ball example

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Screen from './src/Screen'
-import BallScreen from './src/BallScreen'
 
 export default class App extends React.Component {
   render() {
     return (
       <Screen />
-      // <BallScreen />
     );
   }
 }
